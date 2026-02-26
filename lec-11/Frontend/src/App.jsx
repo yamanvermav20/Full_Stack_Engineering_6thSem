2608e85d8ac70940705002708ce7cec0af6ed717
yamanvermav20 <yamanverma083@gmail.com>
2026-02-26 12:47:39 +0530

    Small task random number

--- lec-11/Frontend/src/App.jsx
+++ lec-11/Frontend/src/App.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
   const [oldnumber, newnumber] = useState(null);
   const [answer, newanswer] = useState("");
 
-  function handleClick(){
-    const num = Math.floor(Math.random * 10) + 1;
+  function handleClick() {
+    const num = Math.floor(Math.random() * 10) + 1;
     newnumber(num);
-    if(num == 7){
+
+    if (num === 7) {
       newanswer("You won");
-    }
-    else{
+    } else {
       newanswer("Your lost");
     }
   }
 
   return (
     <div>
-      <h1>Hello</h1>;
-      <button onClick = {handleClick}>clickme</button>;
-      <h1>{newnumber} + {newanswer}</h1>
-    </div>
+      <h1>Hello</h1>
+
+      <button onClick={handleClick}>clickme</button>
+
+      <h1>{oldnumber} {answer}</h1>
+
+      {answer === "You won" && (<img src="https://cdn-icons-png.flaticon.com/512/190/190411.png" />)}
+      {answer === "Your lost" && (<img src="https://cdn-icons-png.flaticon.com/512/1828/1828843.png"/>)}</div>
   )
 }
 
